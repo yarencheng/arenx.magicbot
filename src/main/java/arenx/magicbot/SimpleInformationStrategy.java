54a6ee3563782c68c8c75418c7006658f7a4759c
Aren Chen <yarencheng@gmail.com>
2016-08-21 13:35:27 +0800

    show hourly exp

--- src/main/java/arenx/magicbot/SimpleInformationStrategy.java
+++ src/main/java/arenx/magicbot/SimpleInformationStrategy.java
@@ -18,12 +18,26 @@ public class SimpleInformationStrategy implements InformationStrategy{
 	@Autowired
 	private AtomicReference<PokemonGo> go;
 
-	private long lastShowTIme = 0;
+	private long startTime;
+	private long startExp;
+
 
 	@Override
 	public void showStatus() {
 
-		if (System.currentTimeMillis() - lastShowTIme < 5 * 60 * 1000) {
+
+
+		showExp();
+
+
+
+	}
+
+	private long lastTimeshowExp = 0;
+
+	private void showExp() {
+
+		if (System.currentTimeMillis() - lastTimeshowExp < 1 * 60 * 1000) {
 			return;
 		}
 
@@ -32,12 +46,26 @@ public class SimpleInformationStrategy implements InformationStrategy{
 
 		int exp_percent = (int)((double)(stats.getExperience() - stats.getPrevLevelXp()) / (double)(stats.getNextLevelXp() - stats.getPrevLevelXp()) * 100);
 
-		logger.info("[Status] {} lv:{}({}%) exp:{}/{}",
+		long exp_hourly = 0;
+
+		if (startTime==0) {
+			startTime=System.currentTimeMillis();
+			startExp=stats.getExperience();
+		} else {
+			exp_hourly = (int)((stats.getExperience() - startExp) / ((double)(System.currentTimeMillis()-startTime) / (60 * 60 * 1000) ));
+			logger.error("aaa {}", (stats.getExperience() - startExp));
+			logger.error("aaa {}", ((double)(System.currentTimeMillis()-startTime) / (60 * 60 * 1000) ));
+		}
+
+
+
+		logger.info("[Status] {} lv:{}({}%) exp:{}/{} exp/h:{}",
 				data.getUsername(),
 				stats.getLevel(), exp_percent,
-				stats.getExperience(),stats.getNextLevelXp());
+				stats.getExperience(),stats.getNextLevelXp(),
+				exp_hourly);
 
-		lastShowTIme = System.currentTimeMillis();
+		lastTimeshowExp = System.currentTimeMillis();
 
 	}
 
